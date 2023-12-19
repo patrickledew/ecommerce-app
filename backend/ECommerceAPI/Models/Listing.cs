f247926c890e2222c4bb445f13e9a3cc06fa0de7
Ricky LeDew <adotout1@gmail.com>
2023-12-19 17:31:39 -0500

    Initialize frontend and backend

---
+++ backend/ECommerceAPI/Models/Listing.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceAPI.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        // public string imageUrl { get; set; }
+    }
+}
